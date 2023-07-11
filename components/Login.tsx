22c33224046564ebb5c36f948e2c9fe0c7c1cb29
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-11 19:41:14 +0530

    login screen & functionallity

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+"use client";
+
+import { signIn } from "next-auth/react";
+import Image from "next/image";
 
 type Props = {};
 
 const Login = (props: Props) => {
-	return <div>Login</div>;
+	return (
+		<div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
+			<Image
+				src="https://links.papareact.com/2i6"
+				width={300}
+				height={300}
+				alt="logo"
+			/>
+			<button
+				className="text-white font-bold text-3xl animate-pulse"
+				onClick={() => signIn("google")}
+			>
+				Sign In to use ChatGPT
+			</button>
+		</div>
+	);
 };
 
 export default Login;
