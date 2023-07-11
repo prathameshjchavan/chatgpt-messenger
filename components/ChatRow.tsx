32c3810659688eb3caffc3e5c09274799c736cd3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-11 20:11:07 +0530

    connecting to cloud firestore database

---
+++ components/ChatRow.tsx
@@ -0,0 +1,9 @@
+type Props = {
+	id: string;
+};
+
+const ChatRow = (props: Props) => {
+	return <div>ChatRow</div>;
+};
+
+export default ChatRow;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,17 @@
 "use client";
 
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
 import NewChat from "./NewChat";
+import { db } from "@/firebase";
+import { collection } from "firebase/firestore";
+import ChatRow from "./ChatRow";
 
 const Sidebar = () => {
 	const { data: session } = useSession();
+	const [chats, loading, error] = useCollection(
+		session && collection(db, "users", session.user?.email!, "chats")
+	);
 
 	return (
 		<div className="p-2 flex flex-col h-screen">
@@ -15,6 +22,9 @@ const Sidebar = () => {
 					<div>{/* Model Selection */}</div>
 
 					{/* Map through the ChatRows */}
+					{chats?.docs.map((chat) => (
+						<ChatRow key={chat.id} id={chat.id} />
+					))}
 				</div>
 			</div>
 
