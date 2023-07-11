133c056836fc194ba4394e8cc317b051b623d6d0
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-11 19:45:45 +0530

    sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
 
 const Sidebar = () => {
+	const { data: session } = useSession();
+
 	return (
-		<div className="p-2 flex flex-col">
+		<div className="p-2 flex flex-col h-screen">
 			<div className="flex-1">
 				<div>
 					<NewChat />
@@ -12,6 +17,16 @@ const Sidebar = () => {
 					{/* Map through the ChatRows */}
 				</div>
 			</div>
+
+			{session && (
+				<img
+					src={session.user?.image!}
+					onClick={() => signOut()}
+					className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50
+			"
+					alt={session.user?.name!}
+				/>
+			)}
 		</div>
 	);
 };
