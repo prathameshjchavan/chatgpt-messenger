52613ba1c324943aedec1e169ed92b7a16718d04
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-07-11 15:37:05 +0530

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,68 @@
+import {
+	BoltIcon,
+	ExclamationTriangleIcon,
+	SunIcon,
+} from "@heroicons/react/24/outline";
+
 export default function Home() {
 	return (
-		<main>
-			<h1 className="font-bold">Let's build ChatGPT Messenger</h1>
+		<main className="flex flex-col items-center justify-center h-screen text-white px-2">
+			<h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+
+			<div className="flex space-x-2 text-center">
+				<div>
+					<div className="flex flex-col items-center justify-center mb-5">
+						<SunIcon className="h-8 w-8" />
+						<h2>Example</h2>
+					</div>
+
+					<div className="space-y-2">
+						<p className="infoText">&quot;Explain Something to me&quot;</p>
+						<p className="infoText">
+							&quot;What is the difference between a dog and a cat?&quot;
+						</p>
+						<p className="infoText">
+							&quot;What is the color of the sun?&quot;
+						</p>
+					</div>
+				</div>
+
+				<div>
+					<div className="flex flex-col items-center justify-center mb-5">
+						<BoltIcon className="h-8 w-8" />
+						<h2>Capabilities</h2>
+					</div>
+
+					<div className="space-y-2">
+						<p className="infoText">Change the ChatGPT Model to use</p>
+						<p className="infoText">
+							Messages are stored in Firebase's Firestore
+						</p>
+						<p className="infoText">
+							Hot Toast notifications when ChatGPT is thinking!
+						</p>
+					</div>
+				</div>
+
+				<div>
+					<div className="flex flex-col items-center justify-center mb-5">
+						<ExclamationTriangleIcon className="h-8 w-8" />
+						<h2>Limitations</h2>
+					</div>
+
+					<div className="space-y-2">
+						<p className="infoText">
+							May occasionally generate incorrect information
+						</p>
+						<p className="infoText">
+							May occasionally produce harmful instructions or biased content
+						</p>
+						<p className="infoText">
+							Limited knowledge of world and events after 2021
+						</p>
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
